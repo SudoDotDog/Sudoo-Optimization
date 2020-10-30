236e74480430b1fdbf15393a6ca836fa286a1726
PCXPY <pc@wmpcxpy.com>
2020-10-30 18:00:58 -0500

    [feat] add options

--- src/particle-swarm.ts
+++ src/particle-swarm.ts
@@ -7,22 +7,26 @@
 import { ConstraintManager } from "./constraint";
 import { Variables } from "./declare";
 
-export type ParticleSwarmOptimizationOptions = {
+export type ParticleSwarmOptimizationOptions<T> = {
 
+    readonly particles: number;
     readonly iterations: number;
+
+    readonly initialization: () => T;
+    readonly function: (variables: T) => number;
 };
 
 export class ParticleSwarmOptimization<T extends Variables> {
 
-    public static create<T extends Variables>(options: ParticleSwarmOptimizationOptions): ParticleSwarmOptimization<T> {
+    public static create<T extends Variables>(options: ParticleSwarmOptimizationOptions<T>): ParticleSwarmOptimization<T> {
 
         return new ParticleSwarmOptimization<T>(options);
     }
 
-    private readonly _options: ParticleSwarmOptimizationOptions;
+    private readonly _options: ParticleSwarmOptimizationOptions<T>;
     private readonly _constraints: ConstraintManager<T>;
 
-    private constructor(options: ParticleSwarmOptimizationOptions) {
+    private constructor(options: ParticleSwarmOptimizationOptions<T>) {
 
         this._options = options;
         this._constraints = ConstraintManager.create<T>();
@@ -30,5 +34,17 @@ export class ParticleSwarmOptimization<T extends Variables> {
 
     public get constraints(): ConstraintManager<T> {
         return this._constraints;
+    }
+
+    public findMinimum(): number {
+
+        const particles: T[] = new Array(this._options.particles).fill(undefined).map(this._options.initialization);
+
+        let globalBest: number = Infinity;
+
+        for (let i = 0; i < this._options.iterations; i++) {
+
+
+        }
     }
 }
--- test/unit/particle-swarm.test.ts
+++ test/unit/particle-swarm.test.ts
@@ -11,13 +11,17 @@ import { ParticleSwarmOptimization } from "../../src";
 
 describe('Given {ParticleSwarmOptimization} Class', (): void => {
 
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
     const chance: Chance.Chance = new Chance('optimization-particle-swarm');
 
     it('should be able to construct', (): void => {
 
         const PSO: ParticleSwarmOptimization<any> = ParticleSwarmOptimization.create({
+
+            particles: 3,
             iterations: 10,
+
+            initialization: () => ({}),
+            function: () => chance.natural(),
         });
 
         expect(PSO).to.be.instanceOf(ParticleSwarmOptimization);
